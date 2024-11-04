fbc8b55686ee947918f010d8d44132df4bcdd6f5
tomikorzu <tomykorzu@icloud.com>
2024-11-04 10:46:27 -0300

    feat(routes/users.js / auth/register.js): add register validations

--- express-validator/index.js
+++ express-validator/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { query, validationResult } from "express-validator";
 
 import userRoutes from "./server/routes/users.js";
 
@@ -9,6 +10,15 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.get("/hello", query("person").notEmpty().escape(), (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return res.send(`Hello, ${req.query.person}!`);
+  }
+
+  res.send({ errors: result.array() });
+});
 
 app.use("/users", userRoutes);
 
--- express-validator/server/routes/users.js
+++ express-validator/server/routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { body } from "express-validator";
 
 import login from "../auth/login.js";
 import register from "../auth/register.js";
@@ -10,7 +11,19 @@ import userPayload from "../middlewares/userPayload.js";
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.post(
+  "/register",
+  [
+    body("username", "Set your username").exists().isLength({ min: 4 }),
+    body("email", "Set your email").exists().isEmail(),
+    body("password", "Set your password").exists().isLength({ min: 8 }),
+    body("fullname", "Set your fullname")
+      .exists()
+      .isLength({ min: 4 })
+      .isString(),
+  ],
+  register
+);
 router.patch("/change-psw/:id", verifyToken, userPayload, changePassword);
 
 export default router;
