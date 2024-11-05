da63fc0725a13cd17da80e505fb1a5146edb8293
tomikorzu <tomykorzu@icloud.com>
2024-11-05 11:32:35 -0300

    feat(logger.js): add the main logger

--- express-validator/index.js
+++ express-validator/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import userRoutes from "./server/routes/users.js";
+import logger from "./server/config/logger.js";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use("/users", userRoutes);
+
+logger.info("Hello world");
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
--- express-validator/server/config/logger.js
+++ express-validator/server/config/logger.js
@@ -1,16 +1,44 @@
 import winston from "winston";
+const { combine, timestamp, json, prettyPrint, errors, colorize } =
+  winston.format;
+
+const custom = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+  },
+  colors: {
+    error: "red",
+    warn: "yellow",
+    info: "green",
+    http: "blue",
+  },
+};
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: combine(
+    timestamp(),
+    json(),
+    prettyPrint(),
+    errors({ stack: true }),
+    colorize({ all: true })
+  ),
   defaultMeta: { service: "user-service" },
   transports: [
+    new winston.transports.Console(),
     new winston.transports.File({
-      filename: "../logs/error.log",
+      filename: "./express-validator/server/logs/error.log",
       level: "error",
     }),
-    new winston.transports.File({ filename: "../logs/combined.log" }),
   ],
 });
+
+winston.addColors(custom.colors);
+
+logger.info("Hello world");
+logger.error("Error message");
 
 export default logger;
